27d5393d67be38d5d8ed08b9939ccf85ed89c706
obsiddev <jo.piechl@outlook.com>
2023-12-17 19:23:35 +0100

    websit

---
+++ func.js
@@ -0,0 +1,3 @@
+document.addEventListener("selectstart", (event) => event.preventDefault())
+document.addEventListener('contextmenu', (event) => event.preventDefault())
+document.addEventListener('dragstart', (event) => event.preventDefault())
